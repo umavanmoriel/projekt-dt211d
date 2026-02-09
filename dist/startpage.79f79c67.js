48f64d2e229dcff5a84e0c79ff4061304315608e
umavanmoriel <umeda19971102@gmail.com>
2026-02-09 19:58:47 +0100

    -- Lägger till .heading-container i main -- Lägger till google-fonts och stilar för .heading-container -- Lägger till allmäna stilar för h1-h6 och p Lägger till stilar i _reset.scss för att nollställa standartstilar för länkar och bild

--- dist/startpage.79f79c67.js
+++ dist/startpage.79f79c67.js
@@ -94,7 +94,54 @@
 
     function localRequire(x) {
       var res = localRequire.resolve(x);
-      return res === false ? {} : newRequire(res);
+      if (res === false) {
+        return {};
+      }
+      // Synthesize a module to follow re-exports.
+      if (Array.isArray(res)) {
+        var m = {__esModule: true};
+        res.forEach(function (v) {
+          var key = v[0];
+          var id = v[1];
+          var exp = v[2] || v[0];
+          var x = newRequire(id);
+          if (key === '*') {
+            Object.keys(x).forEach(function (key) {
+              if (
+                key === 'default' ||
+                key === '__esModule' ||
+                Object.prototype.hasOwnProperty.call(m, key)
+              ) {
+                return;
+              }
+
+              Object.defineProperty(m, key, {
+                enumerable: true,
+                get: function () {
+                  return x[key];
+                },
+              });
+            });
+          } else if (exp === '*') {
+            Object.defineProperty(m, key, {
+              enumerable: true,
+              value: x,
+            });
+          } else {
+            Object.defineProperty(m, key, {
+              enumerable: true,
+              get: function () {
+                if (exp === 'default') {
+                  return x.__esModule ? x.default : x;
+                }
+                return x[exp];
+              },
+            });
+          }
+        });
+        return m;
+      }
+      return newRequire(res);
     }
 
     function resolve(x) {
@@ -160,11 +207,11 @@
       });
     }
   }
-})({"jS0Vr":[function(require,module,exports,__globalThis) {
+})({"faEYO":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 58886;
+var HMR_SERVER_PORT = 56108;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
@@ -668,6 +715,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"7HB8V":[function(require,module,exports,__globalThis) {
 
-},{}]},["jS0Vr","7HB8V"], "7HB8V", "parcelRequireb624", {})
+},{}]},["faEYO","7HB8V"], "7HB8V", "parcelRequireb624", {})
 
 //# sourceMappingURL=startpage.79f79c67.js.map
